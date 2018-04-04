b8a5b04b43ccd63fc9243ad8b02ded36020da2f0
Christopher Agius <chrisagius@fastmail.com>
2018-04-04 02:46:24 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ function nowServing() {
   if (katzDeli.length < 1) {
     return "There is nobody waiting to be served!";
   } else {
-    let currentCustomer = katzDeli.unshift();
+    let currentCustomer = katzDeli.shift();
     return `Currently serving ${currentCustomer}.`;
   }
 }
